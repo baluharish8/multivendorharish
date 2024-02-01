159d0ced215eb502ee51a7d0bae0151ba17de4d2
Harish <baluharish8@gmail.com>
2024-02-01 11:28:30 +0530

    These changes are done

--- src/App.router.js
+++ src/App.router.js
@@ -2,10 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { useState } from "react";
 
 
 export function AppRouter(){
-
+// useState
     return(
         <>
         <Routes>
